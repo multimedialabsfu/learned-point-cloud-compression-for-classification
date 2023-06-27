2e3bddf10c4cad0aaf87433e12cbd63243cf6db7
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-06-27 00:41:07 -0700

    feat: src.runners.base.PACKAGES add self

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -1,5 +1,7 @@
 # from compressai_trainer.runners import *
 
+from . import base as base
+from . import utils as utils
 from .classification_point_cloud import PointCloudClassificationRunner
 from .multitask_point_cloud_compression import MultitaskPointCloudCompressionRunner
 from .point_cloud_compression import PointCloudCompressionRunner
---
+++ src/runners/base.py
@@ -0,0 +1,6 @@
+import compressai
+import compressai_trainer
+import compressai_trainer.runners.base as _M
+import src
+
+_M.PACKAGES = [compressai, compressai_trainer, src]
