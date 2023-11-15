e771c7f2c6a63dd1e4cbafb523c81fd2ec6b8927
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-11-14 20:23:17 -0800

    refactor: rename runners.* to "pc" and "pcc" (= pc + compression)

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -2,9 +2,9 @@
 
 from . import base as base
 from . import utils as utils
-from .classification_point_cloud import PointCloudClassificationRunner
-from .multitask_point_cloud_compression import MultitaskPointCloudCompressionRunner
-from .point_cloud_compression import PointCloudCompressionRunner
+from .pc_classification import PointCloudClassificationRunner
+from .pcc_multitask import MultitaskPointCloudCompressionRunner
+from .pcc_reconstruction import PointCloudCompressionRunner
 
 __all__ = [
     "MultitaskPointCloudCompressionRunner",
