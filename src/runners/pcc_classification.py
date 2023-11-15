504a86e25bb5208250a2a7a914fc7ccd61583f58
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-11-14 20:27:46 -0800

    refactor: add runners.pcc_classification

---
+++ src/runners/pcc_classification.py
@@ -0,0 +1,7 @@
+"""
+Please use the multi-task runner instead:
+``src.runners.pcc_multitask.MultitaskPointCloudCompressionRunner``.
+That includes both classification and optional reconstruction.
+"""
+
+from .pcc_multitask import MultitaskPointCloudCompressionRunner
