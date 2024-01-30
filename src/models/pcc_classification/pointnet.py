f6fc70726c323e1463551aa2d2ae979f05888564
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-01-30 03:35:45 -0800

    refactor: remove um-pcc-cls-only-pointnet model
    Redundant; already have um-pcc-cls-only-pointnet-mmsp2023.

--- src/models/pcc_classification/pointnet.py
+++ src/models/pcc_classification/pointnet.py
@@ -17,46 +17,6 @@ from src.layers.pcc.pointnet import (
 from .base import BaseClassificationPccModel
 
 
-@register_model("um-pcc-cls-only-pointnet")
-class PointNetClassOnlyPccModel(BaseClassificationPccModel):
-    latent_codec: Mapping[str, LatentCodec]
-
-    def __init__(
-        self,
-        num_points=1024,
-        num_classes=40,
-        num_channels={
-            "g_a": [3, 64, 64, 64, 128, 1024],
-            "task_backend": [1024, 512, 256, 40],
-        },
-        groups={
-            "g_a": [1, 1, 1, 1, 1],
-        },
-    ):
-        super().__init__()
-
-        assert num_channels["task_backend"][0] == num_channels["g_a"][-1]
-        assert num_channels["task_backend"][-1] == num_classes
-
-        self.g_a = pointnet_g_a_simple(num_channels["g_a"], groups["g_a"])
-
-        self.task_backend = pointnet_classification_backend(
-            num_channels=num_channels["task_backend"],
-        )
-
-        self.latent_codec = nn.ModuleDict(
-            {
-                "y": EntropyBottleneckLatentCodec(
-                    channels=num_channels["g_a"][-1],
-                    tail_mass=1e-4,
-                ),
-            }
-        )
-
-        self.outputs = {}
-        self._setup_hooks()
-
-
 @register_model("um-pcc-cls-only-pointnet-mmsp2023")
 @register_model("um-pcc-cls-only-pointnet-mini-001")  # NOTE: Old name.
 class PointNetClassOnlyPccModelMmsp2023(BaseClassificationPccModel):
